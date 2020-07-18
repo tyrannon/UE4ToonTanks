88cab9d79d0ed5dbe5d6c8bf5096eb15685ef08e
Kaiya Kramer <tyrannon@gmail.com>
2020-07-18 09:29:31 -0700

    Added Health Components

---
+++ Components/HealthComponent.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+#include "ToonTanks/GameModes/TankGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	Health = DefaultHealth;
+
+
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GameModeRef = Cast<ATankGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+
+	Owner = GetOwner();
+	if (Owner)
+	{
+		Owner->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeDamage);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Health component has no reference to the Owner"));
+	}
+}
+
+
+void UHealthComponent::TakeDamage(AActor* DamagedActor, float Damage, const class UDamageType, class AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (Damage == 0)
+	{
+		return;
+	}
+
+	Health = FMath::Clamp(Health - Damage, 0.0f, DefaultHealth);
+
+	if (Health <= 0)
+	{
+		if (GameModeRef)
+		{
+			GameModeRef->ActorDied(Owner);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Health component has no reference to the Owner"));
+		}
+		
+	}
+}
